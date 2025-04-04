45312757a639c87e611cb7c90c523c1c4da6e337
Ouki13 <m13moreaulol@gmail.com>
2025-04-04 10:46:10 -0400

    update obstacle

--- Assets/SpawnZone.cs
+++ Assets/SpawnZone.cs
@@ -29,6 +29,18 @@ public class SpawnZone : MonoBehaviour
 
     void SpawnObstacle()
     {
+
+        //GameObject prefabUsed = ObstaclePrefab[Random.Range(0, ObstaclePrefab.Length)];
+        //GameObject instantiated = Instantiate(prefabUsed);
+        //// Vérification
+        //if (prefabUsed == ObstaclePrefab[0])
+        //{
+        //    Debug.Log("Prefab 0 utilisé");
+        //}
+        //else if (prefabUsed == ObstaclePrefab[1])
+        //{
+        //    Debug.Log("Prefab 1 utilisé");
+        //}
         GameObject instantiated = Instantiate(ObstaclePrefab[Random.Range(0, ObstaclePrefab.Length)]);
 
         instantiated.transform.position = new Vector2(
